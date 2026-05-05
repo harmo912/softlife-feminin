63b19bf626cf6bd5ea26defcd138dd20385eb2e2
Harmo <harmohlb01@gmail.com>
2026-05-05 12:49:09 +0100

    fix auth session type

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -39,7 +39,7 @@ export const authOptions: NextAuthOptions = {
       return token
     },
     async session({ session, token }) {
-      if (token) session.user.id = token.id as string
+      if (token && session.user) session.user.id = token.id as string
       return session
     },
   },
